1d6f6ba08727ba9f5c663fc129633923523671fe
root <root@DESKTOP-NRR7F6C.localdomain>
2021-11-05 19:07:43 +0530

    Added a customer page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
     render "new"
+  end
+
+  def index
+    render plain: User.order(:id).map { |user| user.to_pleasent_string }.join("\n")
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+    )
+    if user.save
+      session[:current_user_id] = user.id
+      redirect_to customers_path
+    else
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
